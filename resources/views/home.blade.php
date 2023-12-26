f4f6cd3aefa5aa78a67fb3ee61fa719054bea821
Dominik Kowalski <dominikkowalski331@gmail.com>
2023-12-26 22:44:40 +0100

    Created CRUD functionality

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,62 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Testing123</h1>
+
+    @auth
+       <p>Congrats, you are logged in</p> 
+       <form action="/logout" method="POST">
+        @csrf
+        <button>Logout</button>
+        </form>
+
+        <div style="border: 3px solid black;">
+            <h2>Create a New Post</h2>
+            <form action="/create-post" method="POST">
+                @csrf
+                <input type="text" name="title" placeholder="post title">
+                <textarea name="body" placeholder="body content..."></textarea>
+                <button>Create New Post</button>
+            </form>
+        </div>
+
+        <div style="border: 3px solid black;">
+            <h2>All Posts</h2>
+            @foreach($posts as $post)
+            <div style="background-color: gray; padding: 10px; margin: 10px;">
+                <h3>{{$post['title']}} by {{$post->user->name}}</h3>
+                <p>{{$post['body']}}</p>
+                <p><a hrefk="/edit-post/{{$post->id}}">Edit</a></p>
+                <form action="/delete-post/{{$post->id}}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button>DELETE</button>
+                </form>
+            </div>
+            @endforeach
+        </div>
+
+    @else 
+    <div style="border: 3px solid black;">
+        <h2>Register</h2>
+        <form action="/register" method="POST">
+            @csrf
+            <input name="name" type="text" placeholder="name">
+            <input name="email" type="text" placeholder="email">
+            <input name="password" type="password" placeholder="password">
+            <button>Register</button>
+        </form>
+    </div>
+    <div style="border: 3px solid black;">
+        <h2>Log in</h2>
+        <form action="/login" method="POST">
+            @csrf
+            <input name="loginname" type="text" placeholder="name">
+            <input name="loginpassword" type="password" placeholder="password">
+            <button>Log in</button>
+        </form>
+    </div>
+    @endauth
+
+
 </body>
 </html>
